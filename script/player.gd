860b8c44350dab23302e920f11afdeb84289dc8f
sameer3841 <81244405+sameer3841@users.noreply.github.com>
2024-10-25 09:31:20 -0400

    added attack animations and adjusted animation initiation within the player script

--- script/player.gd
+++ script/player.gd
@@ -7,55 +7,42 @@ var coin_count = 0
 
 func _process(delta):
 	check_fall_off_map()
+	
 func check_fall_off_map():
-	# Set the Y position threshold, for example, if the player falls below Y = 500
 	if position.y > 1000:
 		respawn()
 
 func respawn():
-	# Reset the player's position to the respawn point
 	position = respawn_point
-	velocity = Vector2.ZERO # Reset any velocity to stop the player from moving immediately after respawn
+	velocity = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		$AnimatedSprite2D.play("jump")
 		velocity.y = JUMP_VELOCITY
 		
-	if Input.is_action_just_pressed("jump"):
-		$AnimatedSprite2D.play("jump")
-		
-	if is_on_floor():
-		$AnimatedSprite2D.play("idle")
-		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var multi = 1
 	if Input.is_action_pressed("run"):
-		
 		multi = 2
 	else:
-		
 		multi = 1
-		
-		
+
 	var direction := Input.get_axis("left", "right")
-	if direction:
+	if direction != 0:
 		velocity.x = direction * SPEED * multi
+		if is_on_floor():
+			$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = velocity.x < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED * multi)
+		if is_on_floor():
+			$AnimatedSprite2D.play("idle")
 	
-	if velocity.x < 0:
-		$AnimatedSprite2D.flip_h = true
-	if velocity.x > 0:
-		$AnimatedSprite2D.flip_h = false
-	if velocity.x != 0:
-		$AnimatedSprite2D.play("run")
-	else:
-		$AnimatedSprite2D.play("idle")
-
+	if not is_on_floor():
+		$AnimatedSprite2D.play("jump")
+	
 	move_and_slide()
