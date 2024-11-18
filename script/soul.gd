ccdd44cb060b29b6ab08e7633e7ae49c8963a1ae
joewil08 <joewilson.jmw@gmail.com>
2024-11-18 09:51:08 -0500

    GUI for number of souls collected

--- script/soul.gd
+++ script/soul.gd
@@ -43,6 +43,6 @@ func _on_timer_timeout() -> void:
 
 # TODO: Currently, it is only collected via sword swing. Figure out how to collect it by walking into it.
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	# increase number of collected souls by one
-	#queue_free()
-	pass
+	if area.is_in_group("player_sword"):
+		Inventory.collect_soul()
+		queue_free()
